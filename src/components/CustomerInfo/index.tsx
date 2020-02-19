78f3db6e0a150afab261064205a7f2c0dd8725a7
Kadir Niyazi AÇAR <kadirnacar@kadirnacar.com>
2020-02-19 19:52:17 +0300

    .

--- src/components/CustomerInfo/index.tsx
+++ src/components/CustomerInfo/index.tsx
@@ -43,43 +43,52 @@ class CustomerInfoComp extends Component<Props, CustomerState> {
                     padding: 10,
                     marginHorizontal: 5
                 }, this.props.style]}>
-                <React.Fragment>
-                    <View>
-                        <Text style={{ color: colors.borderColor }}>Misafir Adı</Text>
-                        <Text style={{ fontWeight: "bold", fontSize: 18, color: colors.inputTextColor }}>
+                <View style={{ flex: 1 }}>
+                    <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
+                        <Text style={{ color: colors.borderColor, flexDirection: "row", width: "100%" }}>Misafir Adı</Text>
+                        <Text style={{ width: "100%", fontWeight: "bold", fontSize: 18, color: colors.inputTextColor, flexDirection: "row" }}>
                             {this.props.Customer.current ? this.props.Customer.current.NAME + " " + this.props.Customer.current.SURNAME : null}
                         </Text>
-                        <Text style={{ color: colors.borderColor }}>Bakiye</Text>
-                        <Text style={{ color: colors.inputTextColor }}>{Intl.NumberFormat("TR", {
-                            style: "currency",
-                            currency: "TRL"
-                        }).format((this.props.Customer.current ? this.props.Customer.current.BALANCE : 0))}</Text>
+
                     </View>
-                    {this.props.total != null ?
+                    <View style={{ flexDirection: "row" }}>
                         <View style={{
-                            alignContent:"flex-end",
-                            alignItems:"flex-end",
-                            alignSelf:"flex-end"
+                            width:"33%"
                         }}>
-                            <Text style={{ color: colors.borderColor }}>Toplam Tutar</Text>
+                            <Text style={{ color: colors.borderColor }}>Bakiye</Text>
                             <Text style={{ color: colors.inputTextColor }}>{Intl.NumberFormat("TR", {
                                 style: "currency",
                                 currency: "TRL"
-                            }).format((this.props.total ? this.props.total : 0))}</Text>
-                        </View> : null}
+                            }).format((this.props.Customer.current ? this.props.Customer.current.BALANCE : 0))}</Text>
+                        </View>
                         {this.props.total != null ?
-                        <View style={{
-                            alignContent:"flex-end",
-                            alignItems:"flex-end",
-                            alignSelf:"flex-end"
-                        }}>
-                            <Text style={{ color: colors.borderColor }}>Kalan</Text>
-                            <Text style={{ color: colors.inputTextColor }}>{Intl.NumberFormat("TR", {
-                                style: "currency",
-                                currency: "TRL"
-                            }).format((this.props.Customer.current ? this.props.Customer.current.BALANCE : 0)-(this.props.total ? this.props.total : 0))}</Text>
-                        </View> : null}
-                </React.Fragment>
+                            <View style={{
+                                width:"33%",
+                                alignContent: "flex-end",
+                                alignItems: "flex-end",
+                                alignSelf: "flex-end"
+                            }}>
+                                <Text style={{ color: colors.borderColor }}>Toplam Tutar</Text>
+                                <Text style={{ color: colors.inputTextColor }}>{Intl.NumberFormat("TR", {
+                                    style: "currency",
+                                    currency: "TRL"
+                                }).format((this.props.total ? this.props.total : 0))}</Text>
+                            </View> : null}
+                        {this.props.total != null ?
+                            <View style={{
+                                width:"33%",
+                                alignContent: "flex-end",
+                                alignItems: "flex-end",
+                                alignSelf: "flex-end"
+                            }}>
+                                <Text style={{ color: colors.borderColor }}>Kalan</Text>
+                                <Text style={{ color: colors.inputTextColor }}>{Intl.NumberFormat("TR", {
+                                    style: "currency",
+                                    currency: "TRL"
+                                }).format((this.props.Customer.current ? this.props.Customer.current.BALANCE : 0) - (this.props.total ? this.props.total : 0))}</Text>
+                            </View> : null}
+                    </View>
+                </View>
             </TouchableHighlight>
         )
     }
